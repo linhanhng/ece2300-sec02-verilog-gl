520123258c28459e4c950627a6729fd00aa9f914
lan64 <lan64@cornell.edu>
2024-09-08 00:55:06 -0400

    finished implementation

--- PairTripleDetectorGL.v
+++ PairTripleDetectorGL.v
@@ -13,9 +13,13 @@ module PairTripleDetectorGL
   output wire out
 );
 
-  //''' ACTIVITY '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-  // Implement pair/triple detector using explicit gate-level modeling
-  //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  wire w;
+  wire x;
+  wire y;
+  or(w, in0, in1);
+  and(x, in0, in1);
+  and(y, w, in2);
+  or(out, y, x);
 
 endmodule
 
